05305bd97841e69823a8e6768b6d9387c5b8df3b
JackieLin2004 <linxy20040304@163.com>
2025-03-08 16:29:45 +0800

    SpringCloud basic environment setup

---
+++ services/test-service/src/main/java/ynu/jackielinn/test/TestMainApplication.java
@@ -0,0 +1,11 @@
+package ynu.jackielinn.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestMainApplication.class, args);
+    }
+}
---
+++ services/test2-service/src/main/java/ynu/jackielinn/test2/Test2MainApplication.java
@@ -0,0 +1,11 @@
+package ynu.jackielinn.test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Test2MainApplication.class, args);
+    }
+}
