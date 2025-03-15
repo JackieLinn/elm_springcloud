543e90b5f1fe93af98631c4f7b14c65fc4a9a9ad
JackieLin2004 <linxy20040304@163.com>
2025-03-15 15:02:17 +0800

    Object conversions are written in a uniform way

--- services/auth-service/src/main/java/ynu/jackielinn/auth/utils/AuthConvertUtils.java
+++ services/auth-service/src/main/java/ynu/jackielinn/auth/utils/AuthConvertUtils.java
@@ -4,7 +4,7 @@ import ynu.jackielinn.auth.dto.response.AccountVO;
 import ynu.jackielinn.auth.entity.Account;
 
 /**
- * VO对象转换工具
+ * VO 对象转换器
  */
 public class AuthConvertUtils {
 
@@ -17,12 +17,12 @@ public class AuthConvertUtils {
         if (account == null) {
             return null;
         }
-        AccountVO vo = new AccountVO();
-        vo.setUserId(account.getUserId());
-        vo.setUserName(account.getUserName());
-        vo.setUserSex(account.getUserSex());
-        vo.setEmail(account.getEmail());
-        vo.setUserImg(account.getUserImg());
-        return vo;
+        return account.asViewObject(AccountVO.class, vo -> {
+            vo.setUserId(account.getUserId());
+            vo.setUserName(account.getUserName());
+            vo.setUserSex(account.getUserSex());
+            vo.setEmail(account.getEmail());
+            vo.setUserImg(account.getUserImg());
+        });
     }
 }
